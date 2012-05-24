abf46ca2909dc3b6af7a678d868185cbac15c8c5
MartinSchade <martin.schade@bertelsmann.de>
2012-05-24 16:30:30 -0400

    and and 3rd course

--- src/main/java/mh/osgitest/SampleServiceImpl.java
+++ src/main/java/mh/osgitest/SampleServiceImpl.java
@@ -1,7 +1,13 @@
 package mh.osgitest;
 
+
+/**
+ * 
+ * @scr.component
+ * @scr.service interface="SampleService"
+ */
 public class SampleServiceImpl implements SampleService {
 	public String sayHello() {
-		return "hello";
+		return "Hello 3rd World!";
 	}
 }
